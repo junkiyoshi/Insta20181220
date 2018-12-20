c41ab14512dbab59ea16d4b2964494f09ac3b486
中内 純 <nakauchi.k@g-hits.co.jp>
2018-12-20 17:42:10 +0900

    First

---
+++ ofApp.cpp
@@ -0,0 +1,43 @@
+﻿#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofSetLineWidth(1.5);
+
+	this->number_of_particle = 39;
+	for (int i = 0; i < this->number_of_particle; i++) {
+
+		auto particle = make_unique<Particle>();
+		this->particles.push_back(move(particle));
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	for (auto& particle : this->particles) {
+		
+		particle->update(this->particles);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	for (auto& particle : this->particles) {
+
+		particle->draw();
+	}
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
